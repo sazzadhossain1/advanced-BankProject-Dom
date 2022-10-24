0a3ff42dbeba9bdc553f49fb8201487111bda967
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-10-24 16:45:04 +0600

    prepend,append,befor and after method

--- bank.js
+++ bank.js
@@ -28,3 +28,34 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+//////////////////////////////////////////////
+//-----------------------------------------//
+/////////////////////////////////////////////
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector(".header");
+const allSections = document.querySelectorAll(".section");
+console.log(allSections);
+
+document.getElementById("section--1");
+const allButtons = document.getElementsByTagName("button");
+console.log(allButtons);
+
+console.log(document.getElementsByClassName("btn"));
+
+// creating and inserting elements
+// .insertAdjacentHTML
+
+const massage = document.createElement("div");
+massage.classList.add("cookie-message");
+// massage.textContent = "We use cookied for improved functionality and analytics";
+massage.innerHTML = `We use cookied for improved functionality and analytics. <button class = "btn btn--close-cookie">Got it!</button>`;
+// header.prepend(massage);
+header.append(massage);
+// header.append(massage.cloneNode(true));
+
+// header.before(massage);
+// header.after(massage);
